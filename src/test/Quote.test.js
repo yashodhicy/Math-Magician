3685b92ac406210883090d3c60d24eb40c5de75d
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-06-08 18:40:03 +0300

    fix linter errors

--- src/test/Quote.test.js
+++ src/test/Quote.test.js
@@ -1,9 +1,9 @@
 import { render } from '@testing-library/react';
-import Quote from "../Quote";
+import Quote from '../Quote';
 
 describe('Should render as expected', () => {
-    it('should work as expected', () => {
-      const result = render(<Quote />);
-      expect(result).toMatchSnapshot();
-    });
+  it('should work as expected', () => {
+    const result = render(<Quote />);
+    expect(result).toMatchSnapshot();
   });
+});
