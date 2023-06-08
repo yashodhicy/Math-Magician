1a7428884dc4877f015631ba9da5c232a41cfb43
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-06-08 18:35:27 +0300

    add test quote

---
+++ src/test/Quote.test.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import Quote from "../Quote";
+
+describe('Should render as expected', () => {
+    it('should work as expected', () => {
+      const result = render(<Quote />);
+      expect(result).toMatchSnapshot();
+    });
+  });
