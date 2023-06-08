4f80eb4a0a343994783548bb3928a8373bfaeb19
mosidrum <mosiokanga@gmail.com>
2023-06-08 11:13:26 +0100

    Fix linters erros

--- src/test/CalculatorPage.test.js
+++ src/test/CalculatorPage.test.js
@@ -1,9 +1,9 @@
-import { render } from "@testing-library/react";
-import CalculatorPage from '../Calculator'
+import { render } from '@testing-library/react';
+import CalculatorPage from '../Calculator';
 
 describe('Should render as expected', () => {
   it('should work as expected', () => {
     const result = render(<CalculatorPage />);
     expect(result).toMatchSnapshot();
-  })
-})
+  });
+});
--- src/test/Home.test.js
+++ src/test/Home.test.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Home from "../Home";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from '../Home';
 
 describe("Home should render correctly", () => {
   it('should display sentence', () => {
